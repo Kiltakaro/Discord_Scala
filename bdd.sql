9290468f0a3994c8465d49eda98e47a16d8658fd
Fripouney <loanlabastie@gmail.com>
2025-02-04 09:41:01 +0100

    Added default values in database

--- bdd.sql
+++ bdd.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS User (
     username String,
     password String,
     is_admin UInt8 DEFAULT 0,
-    creation_date DateTime,
-    roles Array(UUID),
-    guilds Array(UUID)
+    creation_date DateTime DEFAULT now(),
+    roles Array(UUID) DEFAULT [],
+    guilds Array(UUID) DEFAULT []
 ) ENGINE = MergeTree
 ORDER BY user_id;
 
@@ -17,9 +17,9 @@ CREATE TABLE IF NOT EXISTS Guild (
     owner_id UUID,
     creation_date DateTime,
     members Array(UUID),
-    roles Array(UUID),
     channels Array(UUID),
-    banned_users Array(UUID)
+    roles Array(UUID) DEFAULT [],
+    banned_users Array(UUID) DEFAULT []
 ) ENGINE = MergeTree
 ORDER BY guild_id;
 
@@ -27,12 +27,12 @@ ORDER BY guild_id;
 CREATE TABLE IF NOT EXISTS Channel (
     channel_id UUID DEFAULT generateUUIDv4(),
     channel_name String,
-    channel_description Nullable(String),
+    channel_description String,
     guild_id UUID,
     is_private UInt8 DEFAULT 0,
-    roles_allowed Array(UUID),
-    members_allowed Array(UUID),
-    messages Array(UUID)
+    roles_allowed Array(UUID) DEFAULT [],
+    members_allowed Array(UUID) DEFAULT [],
+    messages Array(UUID) DEFAULT []
 ) ENGINE = MergeTree
 ORDER BY channel_id;
 
@@ -40,16 +40,26 @@ ORDER BY channel_id;
 CREATE TABLE IF NOT EXISTS DM_Channel (
     dm_channel_id UUID DEFAULT generateUUIDv4(),
     members Array(UUID),
+    messages Array(UUID) DEFAULT []
 ) ENGINE = MergeTree
 ORDER BY dm_channel_id;
+
+CREATE TABLE IF NOT EXISTS DM_Message (
+    dm_message_id UUID DEFAULT generateUUIDv4(),
+    dm_channel_id UUID,
+    sender_id UUID,
+    content String,
+    sent_at DateTime64 DEFAULT now()
+) ENGINE = MergeTree
+ORDER BY sent_at;
 
 
 CREATE TABLE IF NOT EXISTS Message (
     message_id UUID DEFAULT generateUUIDv4(),
     channel_id UUID,
     sender_id UUID,
-    sent_at DateTime64,
     content String
+    sent_at DateTime64 DEFAULT now(),
 ) ENGINE = MergeTree
 ORDER BY sent_at;
 
--- src/main/scala/HttpServer.scala
+++ src/main/scala/HttpServer.scala
@@ -12,22 +12,7 @@ import org.http4s.server.Server
 // On utilise ce fichier là pour lancer le serveur
 object HttpServer {
 
-    // Combiner les routes ! 
-    // faut trouver un moyen de faire un truc comme ça 
-    // val finalHttpApp = Logger.httpApp(true, true)(app)
-    // ====>
-    // app = Admin.httpApp + User.httpApp
-    // val finalHttpApp = Logger.httpApp(true, true) (app)
-    // 
-    // ce truc devrait marcher, ça run mais on peut juste pas acceder aux routes du 2eme
-    
     // https://stackoverflow.com/questions/58446033/how-to-combine-authedroutes-and-httproutes-in-http4s
-    // val app = Admin.httpApp <+> User.httpApp
-    // val finalHttpApp = Logger.httpApp(true, true)(Admin.httpApp <+> User.httpApp)
-
-    // Exactement le meme probleme avec ça 
-    // val app = User.httpApp.combineK(Admin.httpApp)
-    // val finalHttpApp = Logger.httpApp(true, true)(app)
 
     // OK Router fonctionne 
     // https://http4s.org/v1/docs/service.html
