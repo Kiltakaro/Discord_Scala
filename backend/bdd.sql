9a4986f83d34cb898aeeadc0f040254a7cea53dc
Fripouney <loanlabastie@gmail.com>
2025-02-18 16:54:13 +0100

    Updated database file

--- backend/bdd.sql
+++ backend/bdd.sql
@@ -7,8 +7,12 @@ CREATE TABLE IF NOT EXISTS User (
 ) ENGINE = MergeTree
 ORDER BY user_id;
 
--- Il va nous falloir une friend list
-
+CREATE TABLE IF NOT EXISTS Friends (
+    friendship_id UUID,
+    user_id1 UUID,
+    user_id2 UUID
+) ENGINE = MergeTree
+ORDER BY user_id1;
 
 CREATE TABLE IF NOT EXISTS Guild (
     guild_id UUID DEFAULT generateUUIDv4(),
@@ -19,33 +23,13 @@ CREATE TABLE IF NOT EXISTS Guild (
 ) ENGINE = MergeTree
 ORDER BY guild_id;
 
-
 CREATE TABLE IF NOT EXISTS Channel (
-    channel_id UUID DEFAULT generateUUIDv4(),
+    channel_id UUID,
     channel_name String,
-    channel_description String, -- en y repensant ça à l'air overkill une desc pour un channel
-    guild_id UUID,
-    is_private UInt8 DEFAULT 0 -- Jsuis pas sur de ce truc vu qu'on a DM_CHANNEL en dessous
+    friendship_id Nullable(UUID), -- Cas où c'est un channel DM
+    guild_id Nullable(UUID) -- Cas où c'est un channel de guild
 ) ENGINE = MergeTree
 ORDER BY channel_id;
-
--- jpropose Guild_channel relation avec guild
--- et dm_channel, relation entre 2 users
-
-CREATE TABLE IF NOT EXISTS DM_Channel (
-    dm_channel_id UUID DEFAULT generateUUIDv4()
-) ENGINE = MergeTree
-ORDER BY dm_channel_id;
-
-CREATE TABLE IF NOT EXISTS DM_Message (
-    dm_message_id UUID DEFAULT generateUUIDv4(),
-    dm_channel_id UUID,
-    sender_id UUID,
-    content String,
-    sent_at DateTime64 DEFAULT now()
-) ENGINE = MergeTree
-ORDER BY sent_at;
-
 
 CREATE TABLE IF NOT EXISTS Message (
     message_id UUID DEFAULT generateUUIDv4(),
@@ -56,13 +40,6 @@ CREATE TABLE IF NOT EXISTS Message (
 ) ENGINE = MergeTree
 ORDER BY sent_at;
 
-
-CREATE TABLE IF NOT EXISTS Role (
-    role_id UUID DEFAULT generateUUIDv4(),
-    guild_id UUID,
-    role_name String
-) ENGINE = MergeTree
-ORDER BY role_id;
 
 CREATE TABLE IF NOT EXISTS User_Guild (
     user_id UUID,
@@ -76,14 +53,23 @@ CREATE TABLE IF NOT EXISTS Guild_Ban (
 ) ENGINE = MergeTree
 ORDER BY user_id;
 
-CREATE TABLE IF NOT EXISTS User_Role (
-    user_id UUID,
-    role_id UUID
-) ENGINE MergeTree
-ORDER BY user_id;
-
 CREATE TABLE IF NOT EXISTS User_DM_Channel (
     user_id UUID,
     dm_channel_id UUID
+) ENGINE MergeTree
+ORDER BY user_id;
+
+-- Pour le moment on a pas besoin de ces deux dernières tables
+-- Je les laisse au cas où on décide de gérer les rôles au final
+CREATE TABLE IF NOT EXISTS Role (
+    role_id UUID DEFAULT generateUUIDv4(),
+    guild_id UUID,
+    role_name String
+) ENGINE = MergeTree
+ORDER BY role_id;
+
+CREATE TABLE IF NOT EXISTS User_Role (
+    user_id UUID,
+    role_id UUID
 ) ENGINE MergeTree
 ORDER BY user_id;
