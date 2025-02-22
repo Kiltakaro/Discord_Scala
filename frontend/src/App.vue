a2a2bbf6dfbca69bb4c479d2f7235d42fdf8bcf1
Katorrz <morandbrice@gmail.com>
2025-02-22 19:59:25 +0100

    Navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView } from 'vue-router'
+import Navbar from './views/NavBarView.vue'
 </script>
 
 <template>
+  <Navbar />
   <RouterView />
 </template>
 
---
+++ frontend/src/views/NavBarView.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const token = !!localStorage.getItem("token");
+
+const logout = () => {
+    localStorage.removeItem("token");
+    router.push("/");
+};
+
+</script>
+
+<template>
+    <nav class="bg-gray-800 text-white p-4 shadow-md">
+        <!-- Ne s'affiche que si on est login -->
+        <div class="container mx-auto flex justify-between items-center">
+            <div class="flex items-center space-x-6">
+                <router-link to="/" class="text-xl font-bold text-blue-400">
+                    Accueil
+                </router-link>
+
+                <div class="flex space-x-6">
+                    <router-link to="/friends" class="hover:text-blue-400 font-bold transition" v-if="token">
+                        Amis
+                    </router-link>
+
+                    <router-link to="/serverList" class="hover:text-blue-400 font-bold transition" v-if="token">
+                        Serveurs
+                    </router-link>
+
+                    <router-link to="/profile" class="hover:text-blue-400 font-bold transition" v-if="token">
+                        Profil
+                    </router-link>
+                </div>
+            </div>
+
+            <div class="flex space-x-4">
+                <template v-if="token">
+                    <button @click="logout" class="bg-red-500 px-4 py-2 rounded-md hover:bg-red-600 transition">
+                        Déconnexion
+                    </button>
+                </template>
+
+                <template v-else>
+                    <router-link to="/login" class="bg-blue-500 px-4 py-2 rounded-md hover:bg-blue-600 transition">
+                        Connexion
+                    </router-link>
+                    <router-link to="/register"
+                        class="bg-purple-500 px-4 py-2 rounded-md hover:bg-purple-600 transition">
+                        S'inscrire
+                    </router-link>
+                </template>
+            </div>
+        </div>
+    </nav>
+</template>
